9ccf70d547f4ff4591cf87666438ad46a5adb299
Victory-F <victoriia.fuchyzhy@gmail.com>
2023-02-05 11:36:28 +0100

    Added authRouter, login and signup endpoints

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@
 const express = require("express");
 const corsMiddleWare = require("cors");
 
+//routers
+const authRouter = require("./routers/auth");
+
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(corsMiddleWare());
 app.use(express.json());
+
+//routes
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
