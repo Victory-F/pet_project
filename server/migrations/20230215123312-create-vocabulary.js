78900d51694d1e6c4bddca6ffe02424472805f4d
Victory-F <victoriia.fuchyzhy@gmail.com>
2023-02-15 13:37:39 +0100

    Added vocabulary table

---
+++ server/migrations/20230215123312-create-vocabulary.js
@@ -0,0 +1,32 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("vocabularies", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("vocabularies");
+  },
+};
