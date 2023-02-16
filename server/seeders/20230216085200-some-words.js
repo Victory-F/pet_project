1135847e4d9cd791e5d11ca5f540451ec3b5aff3
Victory-F <victoriia.fuchyzhy@gmail.com>
2023-02-16 10:20:14 +0100

    Added words seeder

---
+++ server/seeders/20230216085200-some-words.js
@@ -0,0 +1,105 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "words",
+      [
+        {
+          foreign: "cat",
+          native: "кіт",
+          progress: 20,
+          vocabularyId: 3,
+          categoryId: 1,
+          languageId: 1,
+        },
+        {
+          foreign: "dog",
+          native: "собака",
+          progress: 40,
+          vocabularyId: 3,
+          categoryId: 1,
+          languageId: 1,
+        },
+        {
+          foreign: "bird",
+          native: "птах",
+          progress: 60,
+          vocabularyId: 3,
+          categoryId: 1,
+          languageId: 1,
+        },
+        {
+          foreign: "horse",
+          native: "кінь",
+          progress: 80,
+          vocabularyId: 3,
+          categoryId: 1,
+          languageId: 1,
+        },
+        {
+          foreign: "animal",
+          native: "тварина",
+          progress: 0,
+          vocabularyId: 3,
+          categoryId: 1,
+          languageId: 1,
+        },
+        {
+          foreign: "rabbit",
+          native: "кролик",
+          progress: 0,
+          vocabularyId: 3,
+          categoryId: 1,
+          languageId: 1,
+        },
+        {
+          foreign: "bread",
+          native: "хліб",
+          progress: 20,
+          vocabularyId: 3,
+          categoryId: 3,
+          languageId: 1,
+        },
+        {
+          foreign: "butter",
+          native: "масло",
+          progress: 20,
+          vocabularyId: 3,
+          categoryId: 3,
+          languageId: 1,
+        },
+        {
+          foreign: "jam",
+          native: "варення",
+          progress: 40,
+          vocabularyId: 3,
+          categoryId: 3,
+          languageId: 1,
+        },
+        {
+          foreign: "honey",
+          native: "мед",
+          progress: 20,
+          vocabularyId: 3,
+          categoryId: 3,
+          languageId: 1,
+        },
+        {
+          foreign: "cheese",
+          native: "сир",
+          progress: 20,
+          vocabularyId: 3,
+          categoryId: 3,
+          languageId: 1,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("words", null, {});
+  },
+};
