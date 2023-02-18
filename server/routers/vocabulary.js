a2c0f831d52105f6a59afbc36545944866125535
Victory-F <victoriia.fuchyzhy@gmail.com>
2023-02-18 12:53:36 +0100

    Added get words of vocabulary endpoint

--- server/routers/vocabulary.js
+++ server/routers/vocabulary.js
@@ -3,10 +3,11 @@ const { Router } = require("express");
 const { vocabulary, language } = require("../models");
 
 const authMiddleware = require("../auth/middleware");
+const word = require("../models/word");
 
 const router = new Router();
 
-// Get All Vocabularies
+// Get all Vocabularies
 router.get("/", authMiddleware, async (req, res, next) => {
   try {
     // user making request
@@ -23,11 +24,6 @@ router.get("/", authMiddleware, async (req, res, next) => {
       ],
     });
 
-    // vocabularies exist check
-    if (!allVocabularies) {
-      return res.status(404).send({ message: "Not found" });
-    }
-
     // send all vocabularies
     return res.status(200).send(allVocabularies);
   } catch (e) {
@@ -35,4 +31,42 @@ router.get("/", authMiddleware, async (req, res, next) => {
     next(e);
   }
 });
+
+// Get all Words in Vocabulary
+router.get("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    // user making request
+    const userId = parseInt(req.user.dataValues.id);
+    // vocabulary id
+    const vocabularyId = parseInt(req.params.id);
+
+    //find vocabulary by id where user is owner
+    const vocabById = await vocabulary.findByPk(vocabularyId, {
+      where: { userId: userId },
+    });
+
+    // check if user has the vocabulary
+    if (!vocabById) {
+      return res.status(404).send({ message: "Not found" });
+    }
+
+    // find all words in vocabulary
+    const allWordsInVocab = await word.findAll({
+      where: { vocabularyId: vocabularyId },
+      include: [
+        {
+          model: category,
+          attributes: ["title"],
+        },
+      ],
+    });
+
+    // send all words in vocabulary
+    return res.status(200).send(allWordsInVocab);
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
 module.exports = router;
