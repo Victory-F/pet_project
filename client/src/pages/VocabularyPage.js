fce2ea7b3d627d4b95732d87fda1849241f1cd05
Victory-F <victoriia.fuchyzhy@gmail.com>
2023-02-18 16:45:33 +0100

    Added word to speech functionality

--- client/src/pages/VocabularyPage.js
+++ client/src/pages/VocabularyPage.js
@@ -6,16 +6,31 @@ export const VocabularyPage = () => {
   const token = useSelector(selectToken);
 
   const vocabulary = useSelector(selectVocabulary);
-  console.log(vocabulary);
+  const message = new SpeechSynthesisUtterance();
+
+  const speechHandler = (message, word) => {
+    message.text = word;
+    message.lang = "nl";
+    window.speechSynthesis.speak(message);
+  };
+
   return (
     token && (
       <div>
+        <p>strooplikkerij</p>
+        <button onClick={() => speechHandler(message, "strooplikkerij")}>
+          click
+        </button>
         <h1>Vocabulary Page</h1>
         {vocabulary &&
           vocabulary.map((w) => (
             <div key={w.id}>
               <p>{w.foreign}</p>
+
               <p>{w.native}</p>
+              <button onClick={() => speechHandler(message, w.foreign)}>
+                SPEAK
+              </button>
               <h3>{w.category.title}</h3>
               <br />
             </div>
