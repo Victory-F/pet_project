d24ff8dd7a96a4c59f3ca7302d362484caff33aa
Victory-F <victoriia.fuchyzhy@gmail.com>
2023-02-18 12:53:08 +0100

    Added Open Vocabulary Button

--- client/src/pages/VocabulariesPage.js
+++ client/src/pages/VocabulariesPage.js
@@ -2,12 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectVocabularies } from "../store/vocabularies/selectors";
 import { selectToken } from "../store/user/selectors";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { fetchVocabularies } from "../store/vocabularies/thunks";
 
 export const VocabulariesPage = () => {
   const token = useSelector(selectToken);
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(fetchVocabularies);
   }, [dispatch]);
@@ -17,7 +18,15 @@ export const VocabulariesPage = () => {
     token && (
       <div>
         <h1>VocabulariesPage</h1>
-        {vocabularies && vocabularies.map((v) => <h2>{v.title}</h2>)}
+        {vocabularies &&
+          vocabularies.map((v) => (
+            <div key={v.id}>
+              <h2>{v.title}</h2>{" "}
+              <button onClick={() => navigate("/vocabulary")}>
+                Open Vocabulary
+              </button>
+            </div>
+          ))}
       </div>
     )
   );
