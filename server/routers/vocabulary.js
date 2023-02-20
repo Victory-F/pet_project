f03def14286c19987f70a867ba7c5ff9656375ac
Victory-F <victoriia.fuchyzhy@gmail.com>
2023-02-20 11:18:27 +0100

    Created endpoint to add a vocabulary

--- server/routers/vocabulary.js
+++ server/routers/vocabulary.js
@@ -78,4 +78,56 @@ router.get("/:id", authMiddleware, async (req, res, next) => {
   }
 });
 
+router.post("/", authMiddleware, async (req, res, next) => {
+  try {
+    // user making request
+    const userId = parseInt(req.user.dataValues.id);
+
+    // title and language of vocabulary sent by user
+    const { vocabTitle, vocabLanguage } = req.body;
+
+    //check for parameters provided
+    if (!vocabTitle || !vocabLanguage) {
+      return res.status(400).send("Missing parameters");
+    }
+
+    //find all vocabularies of the user
+    const allVocabularies = await vocabulary.findAll({
+      where: { userId: userId },
+    });
+
+    //find vocabulary with the same name sent by user
+    const vocabularyExists = allVocabularies.find(
+      (v) => v.title.toLowerCase() === vocabTitle.toLowerCase
+    );
+
+    //check the if name of vocabulary sent by user exists
+    if (vocabularyExists) {
+      return res.status(400).send("Vocabulary exists");
+    }
+
+    //find the language in the database
+    const languageExists = language.findOne({
+      where: { title: vocabLanguage },
+    });
+
+    //check if langiage exists
+    if (!languageExists) {
+      return res.status(400).send("Language not found");
+    } else {
+      //create vocabulary
+      const vocabularyAdded = await vocabulary.create({
+        vocabTitle,
+        userId,
+        languageId: languageExists.id,
+      });
+      // send the vocabulary created
+      return res.status(200).send(vocabularyAdded);
+    }
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
 module.exports = router;
